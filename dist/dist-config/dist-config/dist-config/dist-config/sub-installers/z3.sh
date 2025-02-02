8a8934e4436a6b8b57d77166babf80b844d9e25a
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2025-02-02 02:17:40 +0000

    [workflow] expand templates (dfade3ce57d504d2cffe746c967e4a24a4329221)

---
+++ dist/dist-config/dist-config/dist-config/dist-config/sub-installers/z3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+########## AUTO-GENERATED ##########
+# Do not modify this file manually #
+####################################
+VERSION="4.13.3"
+
+set +u
+if [[ ${AC_NO_BUILD_z3} ]]; then exit 0; fi
+set -eu
+
+cd /tmp/ac_install/
+
+echo "::group::Z3"
+
+sudo mkdir -p ./z3/
+
+sudo wget -q "https://github.com/Z3Prover/z3/archive/refs/tags/z3-${VERSION}.tar.gz" -O ./z3.tar.gz
+sudo tar -I pigz -xf ./z3.tar.gz -C ./z3/ --strip-components 1
+
+cd ./z3/
+
+sudo mkdir -p ./build/ && cd ./build/
+
+sudo cmake "${CMAKE_ENVIRONMENT[@]}" \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
+    -DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]}" \
+    ../
+
+sudo cmake --build ./ --target install
+
+echo "::endgroup::"
