85f515b295afd858d801ceae13b8994c027d8a5c
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2025-02-02 04:15:32 +0000

    [workflow] expand templates (ff2c1611220f0134b3348ca24596c9c1c2b63248)

--- dist/sub-installers/range-v3.sh
+++ dist/sub-installers/range-v3.sh
@@ -4,15 +4,21 @@
 ####################################
 VERSION="0.12.0"
 
+set +u
+if [[ ${AC_NO_BUILD_range_v3} ]]; then exit 0; fi
 set -eu
 
-cd /tmp/
+cd /tmp/ac_install/
 
-mkdir -p ./range-v3/
+echo "::group::range-v3"
+
+sudo mkdir -p ./range-v3/
 
 sudo wget -q "https://github.com/ericniebler/range-v3/archive/refs/tags/${VERSION}.tar.gz" -O ./range-v3.tar.gz
 sudo tar -I pigz -xf ./range-v3.tar.gz -C ./range-v3/ --strip-components 1
 
-sudo mkdir -p /opt/range-v3/include/
+sudo mkdir -p /opt/ac_install/include/
 
-sudo cp -Trf ./range-v3/include/ /opt/range-v3/include/
+sudo cp -Trf ./range-v3/include/ /opt/ac_install/include/
+
+echo "::endgroup::"
