50e5e67bff5e9a35f74b0584eb8d20d3e26d115d
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2025-02-03 11:26:55 +0000

    [workflow] expand templates (c5e3e8500e186f39a6168ff83b939a3bd988ad72)

--- dist/sub-installers/gmp.sh
+++ dist/sub-installers/gmp.sh
@@ -2,7 +2,7 @@
 ########## AUTO-GENERATED ##########
 # Do not modify this file manually #
 ####################################
-VERSION="2:6.3.0+dfsg-2ubuntu6"
+VERSION="2:6.3.0+dfsg-2ubuntu6.1"
 
 set +u
 if [[ ${AC_NO_BUILD_gmp} ]]; then exit 0; fi
