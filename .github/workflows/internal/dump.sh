c51eafae97f08a8c82d03496634a544755767e1c
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-02-16 19:57:46 +0900

    docs: fix script summary

---
+++ .github/workflows/internal/dump.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -eu
+
+echo
+echo '## GCC'
+
+if [ -f ./dist/gcc/install.sh ]; then
+    echo '### Install Script'
+    echo '```bash'
+    cat ./dist/gcc/install.sh
+    echo '```'
+fi
+
+if [ -f ./dist/gcc/compile.sh ]; then
+    echo '### Compile Script'
+    echo '```bash'
+    cat ./dist/gcc/compile.sh
+    echo '```'
+fi
+
+if [ -f ./dist/gcc/config.toml ]; then
+    echo '### Configuration File'
+    echo '```toml'
+    cat ./dist/gcc/config.toml
+    echo '```'
+fi
+
+echo
+echo '## Clang'
+
+if [ -f ./dist/clang/install.sh ]; then
+    echo '### Install Script'
+    echo '```bash'
+    cat ./dist/clang/install.sh
+    echo '```'
+fi
+
+if [ -f ./dist/clang/compile.sh ]; then
+    echo '### Compile Script'
+    echo '```bash'
+    cat ./dist/clang/compile.sh
+    echo '```'
+fi
+
+if [ -f ./dist/clang/config.toml ]; then
+    echo '### Configuration File'
+    echo '```toml'
+    cat ./dist/clang/config.toml
+    echo '```'
+fi
