8747bb7b4e7a02dcf23aad39b9312ff79006a16e
uni-kakurenbo <64454054+uni-kakurenbo@users.noreply.github.com>
2025-02-17 11:01:54 +0000

    [workflow] expand templates (6a18edf8151ce30a4ac5cd9dee34f6137f30d03f)

--- dist/gcc/install.sh
+++ dist/gcc/install.sh
@@ -9,9 +9,9 @@ PARALLEL="$(nproc)"
 AC_VARIANT=gcc
 
 BUILD_FLAGS=(
-    "-std=gnu++23"
-    "-w"
-    "-lstdc++exp"
+"-std=gnu++23"
+"-w"
+"-lstdc++exp"
 )
 
 set -eu
@@ -21,8 +21,8 @@ sudo mkdir -p /opt/ac_install/
 
 cd /tmp/ac_install/
 
-if [[ ! -v AC_VARIANT ]] || [[ "${AC_VARIANT}" == "gcc" ]]; then
-    
+if [[ "${AC_VARIANT:-gcc}" == "gcc" ]]; then
+
 # gcc
 (
 VERSION="14.2.0-4ubuntu2~24.04"
@@ -38,10 +38,10 @@ echo "::endgroup::"
 )
 
 
-    CC="gcc-14"
-    CXX="g++-14"
+CC="gcc-14"
+CXX="g++-14"
 else
-    
+
 # clang
 (
 VERSION="19.1.7"
@@ -68,8 +68,8 @@ echo "::endgroup::"
 )
 
 
-    CC="clang-19"
-    CXX="clang++-19"
+CC="clang-19"
+CXX="clang++-19"
 fi
 
 echo "::group::tools"
@@ -77,29 +77,29 @@ sudo apt-get install -y git cmake ninja-build pigz pbzip2
 echo "::endgroup::"
 
 CMAKE_ENVIRONMENT=(
-    -G "Ninja"
+-G "Ninja"
 
-    -DCFLAGS:STRING="-w"
+-DCFLAGS:STRING="-w"
 
-    -DCMAKE_C_COMPILER:STRING="${CC}"
-    -DCMAKE_CXX_COMPILER:STRING="${CXX}"
+-DCMAKE_C_COMPILER:STRING="${CC}"
+-DCMAKE_CXX_COMPILER:STRING="${CXX}"
 
-    -DCMAKE_INSTALL_MESSAGE:STRING="NEVER"
+-DCMAKE_INSTALL_MESSAGE:STRING="NEVER"
 )
 
 BOOST_BUILDER_CONFIG="using gcc : : ${CXX} ;"
 
 if ccache -v; then
-    echo "ccache: enabled"
+echo "ccache: enabled"
 
-    export CCACHE_ENABLED=1
+export CCACHE_ENABLED=1
 
-    CMAKE_ENVIRONMENT+=(
-        -DCMAKE_C_COMPILER_LAUNCHER:STRING="ccache"
-        -DCMAKE_CXX_COMPILER_LAUNCHER:STRING="ccache"
-    )
+CMAKE_ENVIRONMENT+=(
+-DCMAKE_C_COMPILER_LAUNCHER:STRING="ccache"
+-DCMAKE_CXX_COMPILER_LAUNCHER:STRING="ccache"
+)
 
-    BOOST_BUILDER_CONFIG="using ${AC_VARIANT} : : ccache ${CXX} ;"
+BOOST_BUILDER_CONFIG="using ${AC_VARIANT} : : ccache ${CXX} ;"
 fi
 
 export CMAKE_ENVIRONMENT
@@ -127,21 +127,21 @@ cd ./abseil/
 sudo mkdir -p ./build/ && cd ./build/
 
 CMAKE_ARGUMENTS=(
-    "${CMAKE_ENVIRONMENT[@]}"
-    -DABSL_ENABLE_INSTALL:BOOL=ON
-    -DABSL_PROPAGATE_CXX_STD:BOOL=ON
-    -DABSL_USE_SYSTEM_INCLUDES:BOOL=ON
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/
-    -DCMAKE_CXX_FLAGS:STRING="-fPIC ${BUILD_FLAGS[*]}"
+"${CMAKE_ENVIRONMENT[@]}"
+-DABSL_ENABLE_INSTALL:BOOL=ON
+-DABSL_PROPAGATE_CXX_STD:BOOL=ON
+-DABSL_USE_SYSTEM_INCLUDES:BOOL=ON
+-DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/
+-DCMAKE_CXX_FLAGS:STRING="-fPIC ${BUILD_FLAGS[*]}"
 )
 
 if [[ -v AC_RUN_TEST ]] && [[ "${AC_RUN_TEST}" = "true" ]]; then
-    sudo cmake -DABSL_BUILD_TESTING=ON -DABSL_USE_GOOGLETEST_HEAD=ON "${CMAKE_ARGUMENTS[@]}" ../
+sudo cmake -DABSL_BUILD_TESTING=ON -DABSL_USE_GOOGLETEST_HEAD=ON "${CMAKE_ARGUMENTS[@]}" ../
 
-    sudo make "-j${PARALLEL}"
-    sudo ctest --parallel "${PARALLEL}"
+sudo make "-j${PARALLEL}"
+sudo ctest --parallel "${PARALLEL}"
 else
-    sudo cmake "${CMAKE_ARGUMENTS[@]}" ../
+sudo cmake "${CMAKE_ARGUMENTS[@]}" ../
 fi
 
 sudo cmake --build ./ --target install
@@ -187,33 +187,33 @@ sudo tar -I pbzip2 -xf ./boost.tar.bz2 -C ./boost/ --strip-components 1
 cd ./boost/
 
 if [[ -v BOOST_BUILDER_CONFIG ]]; then
-    echo "${BOOST_BUILDER_CONFIG}" | sudo tee -a ./user-config.jam
+echo "${BOOST_BUILDER_CONFIG}" | sudo tee -a ./user-config.jam
 else
-    sudo touch ./user-config.jam
+sudo touch ./user-config.jam
 fi
 
 if [[ -v CCACHE_ENABLED ]]; then
-    sudo sed -i \
-        -e 's/test_compiler g++$TOOLSET_SUFFIX/test_compiler "ccache g++$TOOLSET_SUFFIX"/g' \
-        -e 's/test_compiler clang++$TOOLSET_SUFFIX/test_compiler "ccache clang++$TOOLSET_SUFFIX"/g' \
-        ./tools/build/src/engine/build.sh
+sudo sed -i \
+-e 's/test_compiler g++$TOOLSET_SUFFIX/test_compiler "ccache g++$TOOLSET_SUFFIX"/g' \
+-e 's/test_compiler clang++$TOOLSET_SUFFIX/test_compiler "ccache clang++$TOOLSET_SUFFIX"/g' \
+./tools/build/src/engine/build.sh
 fi
 
 sudo ./bootstrap.sh \
-    --with-toolset="${AC_VARIANT}" \
-    --without-libraries=mpi,graph_parallel \
-    --prefix=/opt/ac_install/
+--with-toolset="${AC_VARIANT}" \
+--without-libraries=mpi,graph_parallel \
+--prefix=/opt/ac_install/
 
 sudo ./b2 \
-    toolset="${AC_VARIANT}" \
-    link=static \
-    threading=single \
-    variant=release \
-    cflags="-w" \
-    cxxflags="${BUILD_FLAGS[*]}" \
-    --user-config="./user-config.jam" \
-    -j"${PARALLEL}" -d0 \
-    install
+toolset="${AC_VARIANT}" \
+link=static \
+threading=single \
+variant=release \
+cflags="-w" \
+cxxflags="${BUILD_FLAGS[*]}" \
+--user-config="./user-config.jam" \
+-j"${PARALLEL}" -d0 \
+install
 
 echo "::endgroup::"
 )
@@ -225,9 +225,9 @@ VERSION="3.4.0-4"
 
 set -eu
 if [[ "${AC_NO_BUILD_eigen:-false}" == true &&
-    "${AC_NO_BUILD_light_gbm:-false}" == true &&
-    "${AC_NO_BUILD_or_tools:-false}" == true ]]; then
-    exit 0
+"${AC_NO_BUILD_light_gbm:-false}" == true &&
+"${AC_NO_BUILD_or_tools:-false}" == true ]]; then
+exit 0
 fi
 
 echo "::group::Eigen3"
@@ -241,13 +241,13 @@ sudo cp -Trf /usr/include/eigen3/ /opt/ac_install/include/
 
 # copy and patch cmake files to build OR-Tools.
 sudo cp -f \
-    /usr/share/eigen3/cmake/Eigen3Targets.cmake \
-    /usr/share/eigen3/cmake/Eigen3Config.cmake \
-    /opt/ac_install/cmake/
+/usr/share/eigen3/cmake/Eigen3Targets.cmake \
+/usr/share/eigen3/cmake/Eigen3Config.cmake \
+/opt/ac_install/cmake/
 
 sudo sed -i \
-    -e 's/include\/eigen3/opt\/ac_install\/include\//g' \
-    /opt/ac_install/cmake/Eigen3Targets.cmake
+-e 's/include\/eigen3/opt\/ac_install\/include\//g' \
+/opt/ac_install/cmake/Eigen3Targets.cmake
 
 sudo apt-get remove -y libeigen3-dev
 
@@ -276,8 +276,8 @@ VERSION="2.6.0"
 
 set -eu
 if [[ "${AC_NO_BUILD_libtorch:-false}" == true ||
-    "${AC_VARIANT:-false}" == "clang" ]]; then
-    exit 0
+"${AC_VARIANT:-false}" == "clang" ]]; then
+exit 0
 fi
 
 cd /tmp/ac_install/
@@ -328,12 +328,12 @@ sudo rm -rf ./external_libs/eigen/
 sudo mkdir -p ./build/ && cd ./build/
 
 sudo cmake "${CMAKE_ENVIRONMENT[@]}" \
-    -DBUILD_CLI:BOOL=OFF \
-    -DBUILD_STATIC_LIB=ON \
-    -DUSE_HOMEBREW_FALLBACK=OFF \
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
-    -DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]} -I/opt/ac_install/include/" \
-    ../
+-DBUILD_CLI:BOOL=OFF \
+-DBUILD_STATIC_LIB=ON \
+-DUSE_HOMEBREW_FALLBACK=OFF \
+-DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
+-DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]} -I/opt/ac_install/include/" \
+../
 
 sudo cmake --build ./ --target install
 
@@ -362,29 +362,29 @@ cd ./or-tools/
 BUILD_TESTING=OFF
 
 if [[ -v AC_RUN_TEST ]] && [[ "${AC_RUN_TEST}" = "true" ]]; then
-    BUILD_TESTING=ON
+BUILD_TESTING=ON
 fi
 
 sudo mkdir -p ./build/ && cd ./build/
 
 sudo cmake "${CMAKE_ENVIRONMENT[@]}" \
-    -DBUILD_ZLIB:BOOL=ON -DBUILD_Protobuf:BOOL=ON -DBUILD_re2:BOOL=ON \
-    -DUSE_COINOR:BOOL=ON -DBUILD_CoinUtils:BOOL=ON -DBUILD_Osi:BOOL=ON -DBUILD_Clp:BOOL=ON -DBUILD_Cgl:BOOL=ON -DBUILD_Cbc:BOOL=ON \
-    -DUSE_GLPK:BOOL=ON -DBUILD_GLPK:BOOL=ON \
-    -DUSE_HIGHS:BOOL=ON -DBUILD_HIGHS:BOOL=ON \
-    -DUSE_SCIP:BOOL=ON -DBUILD_SCIP:BOOL=ON \
-    -DBUILD_SAMPLES:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF \
-    -DBUILD_TESTING:BOOL="${BUILD_TESTING}" \
-    -DCMAKE_PREFIX_PATH:PATH=/opt/ac_install/ \
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
-    -DBUILD_SHARED_LIBS:BOOL=OFF \
-    -DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]}" \
-    ../
+-DBUILD_ZLIB:BOOL=ON -DBUILD_Protobuf:BOOL=ON -DBUILD_re2:BOOL=ON \
+-DUSE_COINOR:BOOL=ON -DBUILD_CoinUtils:BOOL=ON -DBUILD_Osi:BOOL=ON -DBUILD_Clp:BOOL=ON -DBUILD_Cgl:BOOL=ON -DBUILD_Cbc:BOOL=ON \
+-DUSE_GLPK:BOOL=ON -DBUILD_GLPK:BOOL=ON \
+-DUSE_HIGHS:BOOL=ON -DBUILD_HIGHS:BOOL=ON \
+-DUSE_SCIP:BOOL=ON -DBUILD_SCIP:BOOL=ON \
+-DBUILD_SAMPLES:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF \
+-DBUILD_TESTING:BOOL="${BUILD_TESTING}" \
+-DCMAKE_PREFIX_PATH:PATH=/opt/ac_install/ \
+-DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
+-DBUILD_SHARED_LIBS:BOOL=OFF \
+-DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]}" \
+../
 
 sudo cmake --build ./ --config Release --target install
 
 if [[ -v AC_RUN_TEST ]] && [[ "${AC_RUN_TEST}" = "true" ]]; then
-    sudo cmake --build ./ --config Release --target test --parallel "${PARALLEL}"
+sudo cmake --build ./ --config Release --target test --parallel "${PARALLEL}"
 fi
 
 echo "::endgroup::"
@@ -436,8 +436,8 @@ cd ./unordered_dense/
 sudo mkdir -p ./build/ && cd ./build/
 
 sudo cmake "${CMAKE_ENVIRONMENT[@]}" \
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
-    ../
+-DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
+../
 
 sudo cmake --build ./ --target install
 
@@ -466,10 +466,10 @@ cd ./z3/
 sudo mkdir -p ./build/ && cd ./build/
 
 sudo cmake "${CMAKE_ENVIRONMENT[@]}" \
-    -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
-    -DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]}" \
-    ../
+-DCMAKE_BUILD_TYPE:STRING=Release \
+-DCMAKE_INSTALL_PREFIX:PATH=/opt/ac_install/ \
+-DCMAKE_CXX_FLAGS:STRING="${BUILD_FLAGS[*]}" \
+../
 
 sudo cmake --build ./ --target install
 
@@ -478,15 +478,15 @@ echo "::endgroup::"
 
 
 if [ -v ATCODER ]; then
-    echo "::group::finalize"
+echo "::group::finalize"
 
-    find /opt/ac_install/ \
-        -name cmake -or -name pkgconfig -or -name bin -or -name share \
-        -type d -print0 |
-        xargs -0 sudo rm -rf
+find /opt/ac_install/ \
+-name cmake -or -name pkgconfig -or -name bin -or -name share \
+-type d -print0 |
+xargs -0 sudo rm -rf
 
-    sudo apt-get purge -y --auto-remove git cmake ninja-build pigz pbzip2
+sudo apt-get purge -y --auto-remove git cmake ninja-build pigz pbzip2
 
-    echo "::endgroup::"
+echo "::endgroup::"
 fi
 
