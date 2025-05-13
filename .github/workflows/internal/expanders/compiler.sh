6ccf924f46c6381d891904474bffd87381b0af79
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-05-14 00:10:49 +0900

    fix CI

--- .github/workflows/internal/expanders/compiler.sh
+++ .github/workflows/internal/expanders/compiler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
 
-DIST_DIR="./dist/$1"
+VARIANT="$1"
+DIST_DIR="./dist/${VARIANT}"
 
 WORKING_DIRECTORY="$(dirname "$0")/.."
 
@@ -11,7 +12,7 @@ source "${WORKING_DIRECTORY}/functions/flag-generator.sh" "${DIST_DIR}"
 "${WORKING_DIRECTORY}/replacers/pkg-config.sh" "${DIST_DIR}"
 
 gen-flags ac_user | sed -e 's/ +/ /' | tr ' ' '\n' >"${DIST_DIR}/user.flags.txt"
-sed -i '/^$/d' "${DIST_DIR}/user.flags.txt"
+sed -i -e '/^$/d' -e "/^-I\/opt\/atcoder\/${VARIANT}/d" "${DIST_DIR}/user.flags.txt"
 cat "${DIST_DIR}/user.flags.txt"
 
 "${WORKING_DIRECTORY}/replacers/compiler.sh" "${DIST_DIR}"
