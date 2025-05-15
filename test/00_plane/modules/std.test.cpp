f441657e25242ae76d5042c0ffae31c94971b44e
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-05-15 14:47:59 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fix CI

--- test/00_plane/modules/std.test.cpp
+++ test/00_plane/modules/std.test.cpp
@@ -1,21 +1,5 @@
-#include <Eigen/Dense>
 import std;
 
 int main() {
-    Eigen::MatrixXf m(2,4);
-    Eigen::VectorXf v(2);
-
-    m << 1, 23, 6, 9,
-       3, 11, 7, 2;
-
-    v << 2,
-       3;
-
-    Eigen::MatrixXf::Index index;
-    (m.colwise() - v).colwise().squaredNorm().minCoeff(&index);
-
-    std::cout << "Nearest neighbor is column " << index << ":" << std::endl;
-    std::cout << m.col(index) << std::endl;
-
     std::println("{}", "Hello, import std.");
 }
--- test/01_eigen/nearest_neighbor.test.cpp
+++ test/01_eigen/nearest_neighbor.test.cpp
@@ -1,23 +1,22 @@
-int main() {}
-// #include <iostream>
-// #include <Eigen/Dense>
+#include <iostream>
+#include <Eigen/Dense>
 
-// using namespace Eigen;
+using namespace Eigen;
 
-// // ref: https://qiita.com/shiro-kuma/items/0faa02aa064a3c0a460b
-// int main() {
-//   Eigen::MatrixXf m(2,4);
-//   Eigen::VectorXf v(2);
+// ref: https://qiita.com/shiro-kuma/items/0faa02aa064a3c0a460b
+int main() {
+  Eigen::MatrixXf m(2,4);
+  Eigen::VectorXf v(2);
 
-//   m << 1, 23, 6, 9,
-//        3, 11, 7, 2;
+  m << 1, 23, 6, 9,
+       3, 11, 7, 2;
 
-//   v << 2,
-//        3;
+  v << 2,
+       3;
 
-//   MatrixXf::Index index;
-//   (m.colwise() - v).colwise().squaredNorm().minCoeff(&index);
+  MatrixXf::Index index;
+  (m.colwise() - v).colwise().squaredNorm().minCoeff(&index);
 
-//   std::cout << "Nearest neighbor is column " << index << ":" << std::endl;
-//   std::cout << m.col(index) << std::endl;
-// }
+  std::cout << "Nearest neighbor is column " << index << ":" << std::endl;
+  std::cout << m.col(index) << std::endl;
+}
