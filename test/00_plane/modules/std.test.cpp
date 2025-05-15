7c0d1b971e029cc5963bf01d15def3ddb0725e8f
uni_kakurenbo <kohma1116@ybb.ne.jp>
2025-05-15 13:36:30 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fix CI

--- test/00_plane/modules/std.test.cpp
+++ test/00_plane/modules/std.test.cpp
@@ -2,5 +2,20 @@
 import std;
 
 int main() {
+    Eigen::MatrixXf m(2,4);
+    Eigen::VectorXf v(2);
+
+    m << 1, 23, 6, 9,
+       3, 11, 7, 2;
+
+    v << 2,
+       3;
+
+    MatrixXf::Index index;
+    (m.colwise() - v).colwise().squaredNorm().minCoeff(&index);
+
+    std::cout << "Nearest neighbor is column " << index << ":" << std::endl;
+    std::cout << m.col(index) << std::endl;
+
     std::println("{}", "Hello, import std.");
 }
